79dcf1fbd0e123baf48f9d6810cd81f438a6f98d
Wil Permenter <wilpermenter@gmail.com>
2022-03-03 07:54:56 -0600

    Moved OnClick to Btn Div rather than the <p>

--- src/Componets/NavButton.jsx
+++ src/Componets/NavButton.jsx
@@ -4,8 +4,8 @@ import "./Styles/Button.css"
 const NavButton = (props) => {
 
     return(
-        <div className='Button'>
-            <p onClick={()=>{console.log("Ya Clicked a btn")}}>{props.tag}</p>
+        <div onClick={()=>{console.log("Ya Clicked a btn")}} className='Button'>
+            <p>{props.tag}</p>
         </div>
     )
 }
