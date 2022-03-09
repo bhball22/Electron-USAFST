e179a780324c7905578f33559301be8ab8471396
Brandon Ball <74022057+bhball22@users.noreply.github.com>
2022-03-09 11:53:55 -0600

    Arrows Inputed

--- src/Componets/ScrollButton.jsx
+++ src/Componets/ScrollButton.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import "./Styles/Button.css"
+import arrow from "../Assets/icons/scroll-view-arrow.png"
 const ScrollButton = (props) =>  {
 
     function Change(event) {
     props.onChange();
-    console.log(props.tag)
+    console.log(props.tag);
+    console.log(props.direction)
     };
 
+    const button = {
+        background:     'transport',
+        width:          '10%',
+        'margin-left':  '40%',
+        'margin-right': '40%',
+        transform:      'rotate('+(props.direction)+')'
+    }
+
     return(
-        <div onClick={(i)=>Change(i)} className='Button'>
-        <p style={{ userSelect: 'none'}}>{props.tag}</p>
+        <div onClick={(i)=>Change(i)} className='Button' style={button}> 
+        <img src={arrow} alt={props.tag}/>
     </div>
     )
 }
